f9346a3dd85801a189f0c8e67d7edced9b5e737a
Fernando Chirigati <fernando.chirigati@gmail.com>
2016-04-26 15:47:15 -0400

    Adding copyright notice.

--- sigmod16/performance-evaluation/scalability/speedup.py
+++ sigmod16/performance-evaluation/scalability/speedup.py
@@ -1,3 +1,7 @@
+# Copyright (C) 2016 New York University
+# This file is part of Data Polygamy which is released under the Revised BSD License
+# See file LICENSE for full license details.
+
 import os
 import sys
 import math
